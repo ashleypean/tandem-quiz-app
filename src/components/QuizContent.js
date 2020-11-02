0aa990a93c20d7083720ff08004166821dd83c8e
Ashley Pean <pean.ashley@gmail.com>
2020-11-02 04:05:06 -0500

    Optimized displayAnswers function

--- src/components/QuizContent.js
+++ src/components/QuizContent.js
@@ -1,38 +1,10 @@
 import React from 'react'
 import '../styles/QuizContent.css'
+import FinalScore from './FinalScore.js'
 import { useEffect, useState } from 'react'
+import displayAnswers from '../../utils/display-answers'
+const data = require('../questions.json')
 
-
-const setQuestionIndex = (data) => {
-  for(let i = 0; i < data.length; i++) {
-    data[i].number = i + 1
-  }
-  return data
-}
-
-const getRandomNum = () => {
-  return Math.floor(Math.random() * 4) + 1
-}
-
-const randomizeQuestionOrder = (data) => {
-  const answers = data[0].incorrect
-  const correctAnswer = data[0].correct
-  const num = getRandomNum()
-  answers.splice(num, 0, correctAnswer)
-  return answers
-}
-
-const showAnswerChoice = (index, currentAnswers) => {
-  const alpha = ['A', 'B', 'C', 'D']
-  const choice = (
-    <div className="answer-choice">
-    <h3>D</h3>
-    <p className="answer-choice-d">
-      {currentAnswers[3]}
-    </p>
-  </div>
-  )
-}
 
 export default function QuizContent() {
   const [questionList, setQuestionList] = useState([{
@@ -51,56 +23,33 @@ export default function QuizContent() {
   const [selectAnswer, verifyAnswer] = useState('')
 
   useEffect(() => {
-    const data = async () => {
-      await fetch('http://localhost:4000/take')              
-      .then(res => res.json())
-      .then(data => {
-        setQuestionList(setQuestionIndex(data))
-        setCurrentQuestion(data[0])
-        setCurrentPrompt(data[0].question)
-        setCurrentAnswers(randomizeQuestionOrder(data))
-        setQuestionList(setQuestionIndex)
-      })
-      .catch(err => console.log(err))
-    }
-    data()
+    setQuestionList()
+
   }, [])
 
-  const choiceD = (
-  <div className="answer-choice">
-    <h3>D</h3>
-    <p className="answer-choice-d">
-      {currentAnswers[3]}
-    </p>
-  </div>
-  )
+  //Cycle through currentAnswers array and create an answer choice (in JSX) for each question, if 4 answer choices do not exist, truncate the 4th one
+  const displayAnswers = currentAnswers.map((answer, index) => {
+    const alpha = ['A', 'B', 'C', 'D']
+    return (
+      <div className="answer-choice">
+        <h3>{alpha[index]}</h3>
+        <p key={index}> {currentAnswers[index]} </p>
+    </div>
+    )
+    
+  })
+
   return (
     <div className="quiz-content">
       <h3 className="question">
         {currentPrompt}
       </h3>
       <div className="answers">
-        <div className="answer-choice">
-          <h3>A</h3>
-          <p className="answer-choice-a">
-            {currentAnswers[0]}
-          </p>
-        </div>
-        <div className="answer-choice">
-          <h3>B</h3>
-          <p className="answer-choice-b">
-          {currentAnswers[1]}
-          </p>
-        </div>
-        <div className="answer-choice">
-          <h3>C</h3>
-          <p className="answer-choice-c">
-          {currentAnswers[2]}
-          </p>
-        </div>
-          {currentAnswers[3]? (choiceD): null}
-        
+        {displayAnswers}
       </div>
+
+        <button className="next-question">Next &gt;</button>
+        <FinalScore className="final-score"/>
     </div>
   )
 }
